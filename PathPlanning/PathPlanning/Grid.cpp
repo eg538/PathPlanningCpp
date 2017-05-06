03aeffdc32dc1cbf430f34a10e5331803707e956
EmilyGao <eg538@cornell.edu>
2017-05-06 01:32:49 -0400

    Adding variables and methods for Grid

--- PathPlanning/PathPlanning/Grid.cpp
+++ PathPlanning/PathPlanning/Grid.cpp
@@ -10,3 +10,19 @@ Grid::Grid()
 Grid::~Grid()
 {
 }
+
+void Grid::addObstacles(Obstacle obst)
+{
+}
+
+std::vector<Coord> Grid::adjCoords(int x, int y)
+{
+	return std::vector<Coord>();
+}
+
+std::array<int, 2> Grid::intPos(float x, float y)
+{
+	std::array<int, 2> placehold = { 0, 1 };
+	return new std::array<int, 2>;
+}
+
--- PathPlanning/PathPlanning/Grid.h
+++ PathPlanning/PathPlanning/Grid.h
@@ -1,8 +1,33 @@
-#pragma once
+#ifndef GRID_H
+#define GRID_H
+
+#include "Coord.h"
+#include "Obstacle.h"
+
+#include <vector>
+
+using namespace std;
+
 class Grid
 {
 public:
 	Grid();
 	~Grid();
+
+	int nheight;
+	float height;
+	int nwidth;
+	float width;
+	float scale;
+	Coord R2Pos;
+	Coord* map;
+	vector<Obstacle> obstacles;
+
+	void addObstacles(Obstacle obst);
+
+	vector<Coord> adjCoords(int x, int y);
+
+	array<int, 2> intPos(float x, float y);
 };
 
+#endif
